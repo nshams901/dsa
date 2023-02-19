8198ed7472be2af5ddc65dff1c86f22945131883
Nehal Shams <nehalshams786@gmail.com>
2023-02-19 21:35:10 +0530

    assignment1

---
+++ Assignment1.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+
+//1. Write a program to print Hello Students on the screen.
+// int main(){
+//     printf("Hello");
+// }
+
+// 2.
+// int main(){
+//     printf("Hello\n");
+//     printf("Student");
+//     return 0;
+// }
+
+// 3.
+// int main(){
+//     printf("\"My sirG\"");
+// }
+
+
+// 4.
+// int main(){
+//     float r;
+//     printf("Enter radius");
+//     scanf("%f", &r);
+//     float a = 3.14 * r * r;
+//     printf("Area of circle is %f having the radius %f", a, r);
+// }
+
+
+// 5.
+// int main(){
+//     char ch[] = "P";
+//     int i;
+//     for(i = 0; ch[i] != '\0'; ++i);
+
+//     printf("length of the string is %d", i);
+// }
+
+// 6.
+// int main(){
+//     char name[20];
+//     printf("Enter your name ");
+//     scanf("%s", &name);
+//     printf("Your name is: %s", name);
+// }
+
+
+// 7.
+// int main(){
+//     printf("%%d");
+// }
+
+
+// 8.
+// void main(){
+//     printf("\\n");
+// }
+
+
+// 9.
+// void main(){
+//     printf("\\");
+// }
+
+
+// 10
+// int main(){
+//     int d, m, y;
+//     printf("Enter date ");
+//     scanf("%d/%d/%d", &d, &m, &y);
+//     printf("day - %d, month - %d, year - %d", d, m, y);
+// }
+
+
+// 11
+// int main() {
+//     int h,m;
+//     printf( "Enter time in 'HH:MM' format");
+//     scanf("%d:%d", &h, &m);
+//     printf("%d hour and %d minutes", h, m);
+// }
+
+// 12
+// int main() {
+//     int x = printf("ineuron");
+//     printf("%d", x);
+//     return 0;
+// }
+// ineuron7
